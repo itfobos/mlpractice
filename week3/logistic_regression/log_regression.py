1e7780cf8644db3dd20d1b196d5c20d3d60a4a89
ILya <tr.wolfer@gmail.com>
2019-11-17 21:54:03 -0500

    [W3M2] Intermediate commit: Moved all in class
    Added prediction(doesn't work)
    C=0 leads to too big weights

--- week3/logistic_regression/log_regression.py
+++ week3/logistic_regression/log_regression.py
@@ -2,6 +2,7 @@ from math import exp
 
 import numpy as np
 import pandas
+from sklearn.metrics import roc_auc_score
 
 from common.Axis import Axis
 from week3.logistic_regression.data_index import DataIndex
@@ -10,41 +11,75 @@ DATA = pandas.read_csv('./data/data-logistic.csv', header=None, index_col=False)
 ROWS = DATA.values
 
 
-def samples_sum(rows, w, xIndex):
+class LogRegressionClassifier:
+    def __init__(self):
+        self.__w = [0.0, 0.0]
+
+    @staticmethod
+    def __samples_sum(rows, w, x_index):
+        w1, w2 = w
+        return np.apply_along_axis(
+            lambda row: row[DataIndex.Y] * row[x_index] * (
+                    1 - 1 / (1 + exp(-row[DataIndex.Y] * (w1 * row[DataIndex.X1] + w2 * row[DataIndex.X2])))
+            ),
+            axis=Axis.ROW,
+            arr=rows).mean()
+
+    def fit(self, rows, k, w, c, accuracy, iterations_limit):
+        w1, w2 = w
+
+        i = 0
+        while i < iterations_limit:
+            i = i + 1
+            x1_sum = self.__samples_sum(rows, w, DataIndex.X1)
+            x2_sum = self.__samples_sum(rows, w, DataIndex.X2)
+
+            w1_new = w1 + k * x1_sum - k * c * w1
+            w2_new = w2 + k * x2_sum - k * c * w2
+
+            print('w1 w2: %.5f %.5f      sums %.5f %.5f' % (w1_new, w2_new, x1_sum, x2_sum))
+            # print(w1_new, w2_new)
+
+            distance = np.sqrt((w1 - w1_new) ** 2 + (w2 - w2_new) ** 2)
+            if distance < accuracy:
+                print('Distance break')
+                break
+
+            w1, w2 = w1_new, w2_new
+
+        if i == iterations_limit:
+            print('Iterations limit break')
+
+        self.__w = [w1, w2]
+        return self.__w
+
+    def predict(self, rows):
+        w1, w2 = self.__w
+        # a = lambda row: 1 / (1 + exp(-1 * w1 * row[DataIndex.X1] - w2 * row[DataIndex.X2]))
+
+        return np.apply_along_axis(a(self.__w), axis=Axis.ROW, arr=rows)
+
+
+def a(w):
     w1, w2 = w
-    return np.apply_along_axis(
-        lambda row: row[DataIndex.Y] * row[xIndex] * (
-                1 - 1 / (1 + exp(-row[DataIndex.Y] * (w1 * row[DataIndex.X1] + w2 * row[DataIndex.X2])))
-        ),
-        axis=Axis.ROW,
-        arr=rows).mean()
+
+    def a_internal(row):
+        c_pow = (-1 * w1 * row[DataIndex.X1] - w2 * row[DataIndex.X2])
+        print('(x1 x2) = (%.4f %.4f)  c_pow: %.4f' % (row[DataIndex.X1], row[DataIndex.X2], c_pow))
+        return 1 / (1 + exp(c_pow))
+
+    return a_internal
 
 
-def fit_weights(rows, k, w, c, accuracy, iterations_limit):
-    w1, w2 = w
-
-    i = 0
-    while i < iterations_limit:
-        i = i + 1
-        w1_new = w1 + k * samples_sum(rows, w, DataIndex.X1) - k * c * w1
-        w2_new = w2 + k * samples_sum(rows, w, DataIndex.X2) - k * c * w2
-        # print(w1_new, w2_new)
-
-        distance = np.sqrt((w1 - w1_new) ** 2 + (w2 - w2_new) ** 2)
-        if distance < accuracy:
-            print('Distance break')
-            break
-
-        w1, w2 = w1_new, w2_new
-
-    if i == iterations_limit:
-        print('Iterations limit break')
-
-    return [w1, w2]
-
-
-# With normalization
-print(fit_weights(rows=ROWS, k=0.1, c=0, accuracy=0.00001, iterations_limit=10, w=[0.0, 0.0]))
+y_true = ROWS[:, 0]
+clf = LogRegressionClassifier()
 
 # Without normalization
-# print(fit_weights(rows=ROWS, k=0.1, c=0, accuracy=0.00001, iterations_limit=10000, w=[0.0, 0.0]))
+print(clf.fit(rows=ROWS, k=0.1, c=0, accuracy=0.00001, iterations_limit=100, w=[0.0, 0.0]))
+y_scores = clf.predict(ROWS)
+print('Without normalization %.3f' % roc_auc_score(y_true, y_scores))
+
+# With normalization
+# print(clf.fit(rows=ROWS, k=0.1, c=10, accuracy=0.00001, iterations_limit=10000, w=[0.0, 0.0]))
+# y_scores = clf.predict(ROWS)
+# print('With normalization %.3f' % roc_auc_score(y_true, y_scores))
